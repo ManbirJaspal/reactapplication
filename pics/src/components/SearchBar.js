45afe25f9ab76c098b34a00f5d04f1fb0b59c981
manbirsinghjaspal <manb.jaspal@gmail.com>
2019-02-02 13:18:11 -0500

    learnt event handling.

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = {term: 'Hi there!'};
+
+onFormSubmit = (event) => {
+    event.preventDefault(); {/* the event.preventDefault() method keeps the form from submitting automatically and in the process referesh the page*/}
+    console.log(this.state.term);
+}
+
+    
+    
+    render() {
+    return(
+    <div className="ui segment">
+    <form onSubmit={this.onFormSubmit} className="ui form">
+        <div className="field">
+            <label>Image Search</label>
+            <input type="text"
+        value={this.state.term}
+        onChange={(e) => this.setState({term: e.target.value})} /> {/*onChange property checks if the value of the element has been changed or not. As soon as a change is detected we create a call back to onInputChange.
+ */}   
+        </div>
+    </form>
+        
+    </div>
+    );
+}
+}
+
+export default SearchBar;
